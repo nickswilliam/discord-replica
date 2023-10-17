fa05e572fbe7cd7f13694e8a30c3dee2535573a2
nickswilliam <nickswill2012@gmail.com>
2023-10-16 23:22:10 -0300

    adding direct-message section functionalities

---
+++ pages/api/socket/direct-messages/index.ts
@@ -0,0 +1,97 @@
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
+import { NextApiResponseServerIo } from "@/types";
+import { NextApiRequest } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Metodo no permitido" });
+  }
+
+  try {
+    const profile = await currentProfilePages(req);
+
+    const { content, fileUrl } = req.body;
+    const { conversationId } = req.query;
+
+    if (!profile) {
+      return res.status(401).json({ error: "No autorizado" });
+    }
+
+    if (!conversationId) {
+      return res.status(400).json({ error: "Conversation ID no encontrado" });
+    }
+
+    if (!content) {
+      return res.status(400).json({ error: "Contenido no encontrado" });
+    }
+
+    const conversation = await db.conversation.findFirst({
+      where: {
+        id: conversationId as string,
+        OR: [
+          {
+            memberOne: {
+              profileId: profile.id,
+            },
+          },
+          {
+            memberTwo: {
+              profileId: profile.id,
+            },
+          },
+        ],
+      },
+
+      include: {
+        memberOne:{
+            include:{
+                profile: true
+            }
+        },
+        memberTwo:{
+            include:{
+                profile: true
+            }
+        }
+      }
+    });
+
+    if(!conversation){
+        return res.status(404).json({ message: "Conversación no encontrada"}); 
+    }
+
+    const member = conversation.memberOne.profileId === profile.id ? conversation.memberOne : conversation.memberTwo
+    if (!member) {
+      return res.status(404).json({ message: "Miembro no encontrado" });
+    }
+
+    const message = await db.directMessage.create({
+      data: {
+        content,
+        fileUrl,
+        conversationId: conversationId as string,
+        memberId: member.id,
+      },
+      include: {
+        member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    const channelKey = `chat:${conversationId}:messages`;
+
+    res?.socket?.server?.io?.emit(channelKey, message);
+
+    return res.status(200).json(message);
+  } catch (error) {
+    console.log("[DIRECT_MESSAGES_POST]", error);
+    return res.status(500).json({ message: "Internal Error" });
+  }
+}
